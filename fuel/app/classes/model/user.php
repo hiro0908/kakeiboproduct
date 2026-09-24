4c3c11f2abef3a31a1ccf885a6e084acc6dac169
hiro0908 <hiro54924@gmail.com>
2026-09-24 11:29:10 +0900

    コード整形

--- fuel/app/classes/model/user.php
+++ fuel/app/classes/model/user.php
@@ -1,33 +1,33 @@
 <?php
 class Model_User{
     public static function find_by_username($username){
-        return \DB::select("id","username","password")
-        ->from("user")
-        ->where("username","=",$username)
-        ->execute()
-        ->current()?:null;
+        return \DB::select("id", "username", "password")
+        -> from("user")
+        -> where("username", "=", $username)
+        -> execute()
+        -> current() ? : null;
     }
 
     //既に使用された名前の場合の処理
     public static function username_exists($username){
-        return static::find_by_username($username)!==null;
+        return static::find_by_username($username) !== null;
     }
 
     public static function create($username,$password){
         list($id,) = \DB::insert("user")
-        ->set(array(
+        -> set(array(
             "username"   => $username,
             "password"   => password_hash("$password",PASSWORD_DEFAULT),
-            "created_at" => \Date::forge()->format("mysql"),
-            "updated_at" => \Date::forge()->format("mysql")
+            "created_at" => \Date::forge() -> format("mysql"),
+            "updated_at" => \Date::forge() -> format("mysql")
         ))
-        ->execute();
+        -> execute();
         return $id;
     }
     
     public static function delete($id){
         return \DB::delete("user")
-            ->where("id","=",$id)
-            ->execute();
+            -> where("id", "=", $id)
+            -> execute();
     }
 }
